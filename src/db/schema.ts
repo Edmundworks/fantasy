e23e357b36624bee27f005eb8faf12c45c7ae64b
Edmundworks <edmundcuthbert@gmail.com>
2025-07-30 00:11:30 -0400

    fixed

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+// src/db/schema.ts
+import { pgTable, uuid, varchar, integer, decimal, timestamp, boolean, text, date } from 'drizzle-orm/pg-core';
+
+// todo create schema
+// teams
+// matches
+// gameweeks
+// players
+// seasons
